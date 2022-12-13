f1958281113252c0f32200a89ec9613686ed89a0
OtavioVB <otaviovb.developer@gmail.com>
2022-12-13 15:16:52 -0300

    base(archives): `Initial Commit`

---
+++ Olizia.Libraries.DesignPatterns.Adapter.Abstractions/IAdapter.cs
@@ -0,0 +1,21 @@
+﻿namespace Olizia.Libraries.DesignPatterns.Adapter.Abstractions;
+
+/// <summary>
+/// Interface used to implement adapter pattern
+/// </summary>
+/// <typeparam name="TAdapter">An adapter</typeparam>
+/// <typeparam name="TTAdapter">An adapter</typeparam>
+public interface IAdapter<TAdapter, TTAdapter>
+    where TAdapter : class
+    where TTAdapter : class
+{
+    /// <summary>
+    /// Adapter an object to another
+    /// </summary>
+    public TTAdapter Adapt(TAdapter adapter);
+
+    /// <summary>
+    /// Adapter an object to another
+    /// </summary>
+    public TAdapter Adapt(TTAdapter adapter);
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Olizia.Libraries.DesignPatterns.Adapter.Abstractions
+
+This package abstract the Adapter Pattern. Developed by [Otávio Villas Boas](https://www.linkedin.com/in/otaviovillasboas/).
+
+## Adapter Pattern
+
+The adapter pattern is used to transform an object in another object using an adapter object.
